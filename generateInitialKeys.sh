c13fe9a76b5042526a03cfb88d034b7a7466258b
sunsetmountain <brandon@sunsetmountain.com>
2020-09-02 21:23:03 -0600

    Update generateInitialKeys.sh

--- generateInitialKeys.sh
+++ generateInitialKeys.sh
@@ -5,5 +5,3 @@ openssl ecparam -genkey -name prime256v1 -noout -out ec_private_at_bat.pem
 openssl ec -in ec_private_at_bat.pem -pubout -out ec_public_at_bat.pem
 openssl ecparam -genkey -name prime256v1 -noout -out ec_private_on_base.pem
 openssl ec -in ec_private_on_base.pem -pubout -out ec_public_on_base.pem
-#cp ../.ssh/ec_private.pem ec_private_at_bat.pem
-#cp ../.ssh/ec_public.pem ec_public_at_bat.pem
