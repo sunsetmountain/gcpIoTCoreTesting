0621de8f3ca65a5c3e8de08ede8cffa96bf3c3e3
sunsetmountain <brandon@sunsetmountain.com>
2020-09-02 18:42:27 -0600

    Update and rename generate_keys.sh to generateInitialKeys.sh

---
+++ generateInitialKeys.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y wget openssl python3-pip
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private_on_deck.pem
+openssl ec -in ec_private.pem -pubout -out ec_public_on_deck.pem
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private_at_bat.pem
+openssl ec -in ec_private.pem -pubout -out ec_public_at_bat.pem
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private_on_base.pem
+openssl ec -in ec_private.pem -pubout -out ec_public_on_base.pem
