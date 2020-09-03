f0e21ffd27c1acce36ced58afed47a3b0e2b2510
sunsetmountain <brandon@sunsetmountain.com>
2020-09-02 22:41:28 -0600

    Update generateInitialKeys.sh

--- generateInitialKeys.sh
+++ generateInitialKeys.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get install -y wget openssl python3-pip
 openssl ecparam -genkey -name prime256v1 -noout -out ec_private_on_deck.pem
 openssl ec -in ec_private_on_deck.pem -pubout -out ec_public_on_deck.pem
