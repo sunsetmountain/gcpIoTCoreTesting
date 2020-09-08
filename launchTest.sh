a392293da62163401606f8c1c85b6384ce697646
sunsetmountain <brandon@sunsetmountain.com>
2020-09-07 23:18:25 -0600

    Update launchTest.sh

--- launchTest.sh
+++ launchTest.sh
@@ -10,4 +10,8 @@
 # Example:
 # python3 mqttTest.py --algorithm ES256 --ca_certs roots.pem --private_key_file ec_private_at_bat.pem --mqtt_bridge_port 443 --cloud_region us-central1 --project_id my-iotcore-test-project --device_id my-iot-device --registry_id my-test-project-registry
 
-python3 mqttTest.py --algorithm ES256 --ca_certs roots.pem --private_key_file ec_private_at_bat.pem --mqtt_bridge_port 443 --cloud_region cloud-region --project_id project-id --device_id device-id --registry_id registry-id
+# python3 mqttTest.py --algorithm ES256 --ca_certs roots.pem --private_key_file ec_private_at_bat.pem --mqtt_bridge_port 443 --cloud_region cloud-region --project_id project-id --device_id device-id --registry_id registry-id
+
+# or edit the values in config.json and have this script call Python
+
+python3 launch.py
