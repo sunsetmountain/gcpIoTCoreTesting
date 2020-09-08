e349a0c6d16e04b01dd552f1dcf2529a9f557c13
sunsetmountain <brandon@sunsetmountain.com>
2020-09-07 18:52:07 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# get unique identifier for the device
+uniqueID=$(sudo cat /sys/class/dmi/id/product_uuid)
+echo $uniqueID
+echo $uniqueID > device-id.txt
+
+# install software from repository
 sudo apt-get -y install git
 git clone https://github.com/sunsetmountain/gcpIoTCoreTesting
 cd gcpIoTCoreTesting
+
+# run initial setup scripts
 chmod +x generateInitialKeys.sh initialSoftware.sh getGoogleKey.sh launchTest.sh 
 ./initialSoftware.sh
 ./getGoogleKey.sh
 ./generateInitialKeys.sh
-
-uniqueID=$(sudo cat /sys/class/dmi/id/product_uuid)
-echo $uniqueID
-echo $uniqueID > device-id.txt
