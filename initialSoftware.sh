af7eeae3299f896dcb8a574f2129a244a1978e52
sunsetmountain <brandon@sunsetmountain.com>
2020-09-07 18:56:24 -0600

    Update initialSoftware.sh

--- initialSoftware.sh
+++ initialSoftware.sh
@@ -3,3 +3,12 @@
 sudo apt-get update
 sudo apt-get -y install build-essential libssl-dev python3-dev libffi-dev python3-pip git
 sudo pip3 install -r requirements.txt
+
+# download Google keys
+
+# generate initial keys
+sudo apt-get install -y wget openssl python3-pip
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private_on_deck.pem
+openssl ec -in ec_private_on_deck.pem -pubout -out ec_public_on_deck.pem
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private_at_bat.pem
+openssl ec -in ec_private_at_bat.pem -pubout -out ec_public_at_bat.pem
